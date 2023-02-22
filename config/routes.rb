372676237433e9ca83b87ffe8b35e9efa27574c9
helene mercerolle <helenepro@laposte.net>
2023-02-22 16:36:37 +0100

    exo 3 sans le style

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :flats do
+    resources :flats, only: [:show, :edit, :new, :create, :update]
+  end
+  resources :flats, only: [:destroy]
   # Defines the root path route ("/")
+
   # root "articles#index"
 end
